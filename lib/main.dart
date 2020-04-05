925fe9581d25a06cb3b3dc92a27f386fedc0a174
Julius Schulz <julius.schulz@haw-hamburg.de>
2020-04-05 17:47:57 +0200

    update theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+
+        backgroundColor: Color(0xFFFBE6),
+
+        primaryColorBrightness: Brightness.dark,
+        primaryColor: Color(0x5E5E5E),
+        primaryColorLight: Color(0xF3F3F3),
+        primaryColorDark: Color(0x000000),
+
+        cursorColor: Color(0xBC0000),
+        accentColor: Color(0xBC0000),
+        errorColor: Color(0xBC0000),
+
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black),
+          bodyText2: TextStyle(color: Colors.black),
+
+          headline6: TextStyle(color: Colors.black),
+        )
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
