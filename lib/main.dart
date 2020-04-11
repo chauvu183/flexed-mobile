f443cd98a86e4a151feae58e5d973cd4bbd6817e
Julius Schulz <julius.schulz@haw-hamburg.de>
2020-04-11 16:09:45 +0200

    fix bracket

--- lib/main.dart
+++ lib/main.dart
@@ -15,41 +15,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
+        providers: [
 
-        // Values and classes provided here are exposed
-        // to the entire app. We can use this to publish
-        // our services to all widgets where needed!
-        Provider<StudentRepository>(
-          create: (context) => new StudentMockRepository(),
-        )
+          // Values and classes provided here are exposed
+          // to the entire app. We can use this to publish
+          // our services to all widgets where needed!
+          Provider<StudentRepository>(
+            create: (context) => new StudentMockRepository(),
+          )
 
-      ],
+        ],
 
-      child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            brightness: Brightness.light,
+        child: MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              brightness: Brightness.light,
 
-            backgroundColor: Color.fromRGBO(0xFF, 0xFB, 0xE6, 1.0),
+              backgroundColor: Color.fromRGBO(0xFF, 0xFB, 0xE6, 1.0),
 
-            primaryColorBrightness: Brightness.dark,
-            primaryColor: Color.fromRGBO(0x5E, 0x5E, 0x5E, 1.0),
-            primaryColorLight: Color.fromRGBO(0xF3, 0xF3, 0xF3, 1.0),
-            primaryColorDark: Color.fromRGBO(0x00, 0x00, 0x00, 1.0),
+              primaryColorBrightness: Brightness.dark,
+              primaryColor: Color.fromRGBO(0x5E, 0x5E, 0x5E, 1.0),
+              primaryColorLight: Color.fromRGBO(0xF3, 0xF3, 0xF3, 1.0),
+              primaryColorDark: Color.fromRGBO(0x00, 0x00, 0x00, 1.0),
 
-            cursorColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
-            accentColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
-            errorColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
+              cursorColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
+              accentColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
+              errorColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
 
-            textTheme: TextTheme(
-              bodyText1: TextStyle(color: Colors.black),
-              bodyText2: TextStyle(color: Colors.black),
+              textTheme: TextTheme(
+                bodyText1: TextStyle(color: Colors.black),
+                bodyText2: TextStyle(color: Colors.black),
 
-          headline6: TextStyle(color: Colors.black),
-        )
-      ),
-      home: Home()
+            headline6: TextStyle(color: Colors.black),
+          )
+        ),
+        home: Home()
+      )
     );
   }
 }
