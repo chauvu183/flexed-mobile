bb5697543705729b8b4e69cfe969a8cda81294e6
Julius Schulz <julius.schulz@haw-hamburg.de>
2020-04-11 12:48:13 +0200

    add mail type

---
+++ lib/types/mail.dart
@@ -0,0 +1,23 @@
+
+class Mail {
+
+  /// The actual mail value set on construction
+  final String _value;
+
+  /// The pattern the email should match to
+  /// be considered valid
+  RegExp _pattern = new RegExp(
+      r".+\@.+\.[a-zA-Z0-9]{2,}", 
+      caseSensitive: false, 
+      multiLine: false
+    );
+
+
+  Mail(this._value);
+
+
+  bool isValid() {
+    return _pattern.hasMatch(_value);
+  }
+
+}
