3fb6f8f49133d40ff6a3215bee4e6f5b094dfb31
chauvu183 <chau.vu@haw-hamburg.de>
2020-04-11 16:16:49 +0200

    update Router and Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -26,31 +26,31 @@ class MyApp extends StatelessWidget {
 
         ],
 
-        child: MaterialApp(
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              brightness: Brightness.light,
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            brightness: Brightness.light,
 
-              backgroundColor: Color.fromRGBO(0xFF, 0xFB, 0xE6, 1.0),
+            backgroundColor: Color.fromRGBO(0xFF, 0xFB, 0xE6, 1.0),
 
-              primaryColorBrightness: Brightness.dark,
-              primaryColor: Color.fromRGBO(0x5E, 0x5E, 0x5E, 1.0),
-              primaryColorLight: Color.fromRGBO(0xF3, 0xF3, 0xF3, 1.0),
-              primaryColorDark: Color.fromRGBO(0x00, 0x00, 0x00, 1.0),
+            primaryColorBrightness: Brightness.dark,
+            primaryColor: Color.fromRGBO(0x5E, 0x5E, 0x5E, 1.0),
+            primaryColorLight: Color.fromRGBO(0xF3, 0xF3, 0xF3, 1.0),
+            primaryColorDark: Color.fromRGBO(0x00, 0x00, 0x00, 1.0),
 
-              cursorColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
-              accentColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
-              errorColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
+            cursorColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
+            accentColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
+            errorColor: Color.fromRGBO(0xBC, 0x00, 0x00, 1.0),
 
-              textTheme: TextTheme(
-                bodyText1: TextStyle(color: Colors.black),
-                bodyText2: TextStyle(color: Colors.black),
+            textTheme: TextTheme(
+              body1: TextStyle(color: Colors.black),
+              body2: TextStyle(color: Colors.black),
 
-            headline6: TextStyle(color: Colors.black),
-          )
-        ),
-        home: Home()
-      )
+          headline: TextStyle(color: Colors.black),
+        )
+      ),
+      home: Home(),
+    )
     );
   }
 }
