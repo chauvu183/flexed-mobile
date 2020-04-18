1966eae37071f439dbba53f64c6988159b7e73d5
Julius Schulz <julius.schulz@haw-hamburg.de>
2020-04-18 23:00:41 +0200

    implement student carousel

--- lib/pages/feedback/widgets/student_carousel.dart
+++ lib/pages/feedback/widgets/student_carousel.dart
@@ -1,9 +1,15 @@
+import 'package:flexed_mobile/api/repository/soltrack_repository.dart';
 import 'package:flexed_mobile/models/flexclass.dart';
+import 'package:flexed_mobile/models/soltrack.dart';
 import 'package:flexed_mobile/models/student.dart';
 import 'package:flexed_mobile/shared/infocard/info_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
+import 'dart:developer' as developer;
+
+import 'package:provider/provider.dart';
 class StudentCarousel extends StatefulWidget {
 
   final FlexClass flexClass;
@@ -19,10 +25,13 @@ class StudentCarousel extends StatefulWidget {
 class ActiveStudent extends State<StudentCarousel> {
 
   FlexClass flexClass;
-  Student activeStudent;
+  List<Student> _students;
+
+  int _activeStudentIndex;
 
   ActiveStudent(this.flexClass) {
-    activeStudent = flexClass.getMembers().first;
+    _students = flexClass.getMembers();
+    _activeStudentIndex = 0;
   }
 
   @override
@@ -31,9 +40,91 @@ class ActiveStudent extends State<StudentCarousel> {
       appBar: AppBar(
         title : Text(flexClass.title),
       ),
-      body: Card(
-        child: InfoCard(student: activeStudent,),
+      body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(12.0),
+              child: InfoCard(student: _getActiveStudent()),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Card(
+                child: FutureBuilder(
+                  future: _getTrackings(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data == null || snapshot.data.length < 1) {
+                      return Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text('Keine Aufgaben zugewiesen.'),
+                          ),
+                        ],
+                      );
+                    }
+
+                    return Container(
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: _buildTrackingList(snapshot.data)
+                      )
+                    );
+                }),
+              )
+            ),
+          ],
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _nextStudent(),
+        child: Icon(Icons.chevron_right),
+        backgroundColor: Theme.of(context).accentColor,
+        
       ),
     );
+  }
+
+
+  _nextStudent() {
+    if (_activeStudentIndex >= (_students.length - 1)) {
+      return null;
+    }
+
+    setState(() {
+      _activeStudentIndex++;
+    });
+  }
+
+  Student _getActiveStudent() {
+    return _students[_activeStudentIndex];
+  }
+
+  Future<List<SOLTrack>> _getTrackings() {
+    return Provider.of<SOLTrackRepository>(context).byStudents([_getActiveStudent()]);
+  }
+
+
+  _buildTrackingList(List<SOLTrack> trackings) {
+    List<Widget> list = [];
+
+    if (trackings == null) {
+      return list;
+    }
+
+    trackings.forEach((tracking) { 
+      list.add(
+        ListTile(
+          leading: CircleAvatar(
+            child: Icon(Icons.assignment), 
+            backgroundColor: Theme.of(context).primaryColorLight,
+          ),
+          title: Text(tracking.subject.title),
+          subtitle: Text('Begonnen: ' + DateFormat('dd.MM.yyyy').format(tracking.date)),
+        ),
+      );
+    });
+
+    return list;
   }
 }
