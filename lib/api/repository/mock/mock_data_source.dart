4731f6da627aa256f37a8535672a360ec8094fae
Julius Schulz <julius.schulz@haw-hamburg.de>
2020-04-18 20:55:34 +0200

    add random distribution

--- lib/api/repository/mock/mock_data_source.dart
+++ lib/api/repository/mock/mock_data_source.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flexed_mobile/api/mocking/flexclass_mocker.dart';
 import 'package:flexed_mobile/api/mocking/soltrack_mocker.dart';
 import 'package:flexed_mobile/api/mocking/student_mocker.dart';
@@ -21,17 +23,19 @@ class MockDataSource {
   FlexClassMocker _flexClassMocker = FlexClassMocker();
   SOLTrackMocker _solTrackMocker = SOLTrackMocker();
 
+  Random _random = Random();
+
   MockDataSource() {
     flexclasses = _flexClassMocker.mockFlexClasses(number: 7);
 
     flexclasses.forEach((_class) { 
-      for(int i = 0; i < 15; i++) {
+      for(int i = _random.nextInt(15) + 1; i > 0; i--) {
         Student student = _studentMocker.mockStudent();
         students.add(student);
         _class.addMember(student);
       }
 
-      trackings.addAll(_solTrackMocker.mockSOLTracks(flexclass: _class, number: 5));
+      trackings.addAll(_solTrackMocker.mockSOLTracks(flexclass: _class, number: _random.nextInt(10)));
     });
   }
 
