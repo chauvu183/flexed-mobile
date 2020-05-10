9b068cb065e356a97a3b1e2380eb93b1e3523c1c
Clemens Reimer <clemens.reimer@haw-hamburg.de>
2020-05-10 09:36:45 +0200

    added analyzeTrackingsByRating

--- lib/pages/analyze/logic/soltrack_calculator.dart
+++ lib/pages/analyze/logic/soltrack_calculator.dart
@@ -1,5 +1,7 @@
 import 'dart:collection';
 
+import 'package:flexed_mobile/types/enums/rating.dart';
+
 import '../../../models/soltrack.dart';
 
 class SOLTrackCalculator {
@@ -29,6 +31,48 @@ class SOLTrackCalculator {
     result.putIfAbsent('englisch', () => english);
     result.putIfAbsent('mathematik', () => math);
     result.putIfAbsent('total', () => total);
+    return result;
+  }
+
+  static Map<Rating,int> analyzeTrackingsByRating(List<SOLTrack> trackings) {
+    int bad = 0;
+    int quitebad = 0;
+    int average = 0;
+    int quitegood = 0;
+    int good = 0;
+    int total = 0;
+
+    Map<Rating,int> result = HashMap();
+    
+    for (var track in trackings) {
+      switch (track.rating) {
+        case Rating.UNDEFINED:
+          total--;
+          break;
+        case Rating.BAD:
+          bad++;
+          break;
+        case Rating.QUITEBAD:
+          quitebad++;
+          break;
+        case Rating.AVERAGE:
+          average++;
+          break;
+        case Rating.QUITEGOOD:
+          average++;
+          break;
+        case Rating.GOOD:
+          average++;
+          break;
+      }
+      total++;
+    }
+    result.putIfAbsent(Rating.BAD, () => bad);
+    result.putIfAbsent(Rating.QUITEBAD, () => quitebad);
+    result.putIfAbsent(Rating.AVERAGE, () => average);
+    result.putIfAbsent(Rating.QUITEGOOD, () => quitegood);
+    result.putIfAbsent(Rating.GOOD, () => good);
+    
     return result;
   }
 
