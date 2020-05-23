2ef35853db0425c553fce48cce0d3762064b6493
Clemens Reimer <clemens.reimer@haw-hamburg.de>
2020-05-23 13:36:54 +0200

    added new logo to home view

--- lib/pages/solTrackingHomepage/solTrackingHomepage.dart
+++ lib/pages/solTrackingHomepage/solTrackingHomepage.dart
@@ -1,45 +1,22 @@
 import 'package:flutter/material.dart';
 
 class SolTrackingHome extends StatefulWidget {
-
   SolTrackingHome();
   @override
   _SolTrackingHomeState createState() => _SolTrackingHomeState();
 }
 
 class _SolTrackingHomeState extends State<SolTrackingHome> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('FlexEd mobile'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Center(
-              child: ClipOval(
-                child: Container(
-                  color: Color.fromRGBO(0, 0, 0, 0.2),
-                  padding: EdgeInsets.all(10),
-                  child: Image.asset('images/logo512x512.png'),
-                ),
-              ),
-            ),
-          ),
-          Text(
-            'Willkommen bei FlexEd!',
-            style: TextStyle(
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold,
-              fontSize: 26,
-              color: Theme.of(context).primaryColor,
-            ),
-          )
-        ],
-      ),
+      body: Center(
+          child: Image.asset(
+        'images/homepagewithAll-01.png',
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+      )),
     );
   }
 }
